cdf52956df627cf69893009c144f7ba7cf16af95
Samuel Rönnqvist <saanro@utu.fi>
2021-03-08 13:16:28 +0200

    save html to gzip by language

--- warc2corpora.py
+++ warc2corpora.py
@@ -12,12 +12,13 @@ import collections
 
 
 OUTPUT_DIR = sys.argv[2]
-SAMPLING_RATE = 0.1
+#SAMPLING_RATE = 0.1
 
 random.seed(313313)
 
 
-output_files = {}
+output_txt_files = {}
+output_html_files = {}
 filename = sys.argv[1]
 #f = warc.WARCFile(fileobj=gzip.open(filename))
 from warcio.archiveiterator import ArchiveIterator
@@ -30,7 +31,7 @@ cntr = 0
 last_domain = ""
 for i, record in enumerate(ArchiveIterator(f)):
     if i % 1000 == 0:
-        print("%s:%d, %d pages extracted in %d languages" % (filename, i, cntr, len(output_files)))
+        print("%s:%d, %d pages extracted in %d languages" % (filename, i, cntr, len(output_txt_files)))
     #if last_domain == record.rec_headers.get_header('WARC-Target-URI').split('/')[2]:
     #    continue
     #if random.random() > SAMPLING_RATE:
@@ -60,21 +61,30 @@ for i, record in enumerate(ArchiveIterator(f)):
     except lang_detect_exception.LangDetectException:
         pass
 
-    if lang not in output_files:
-        output_files[lang] = gzip.open(os.path.join(OUTPUT_DIR, '%s.txt.gz' % lang), 'at')
+    if lang not in output_txt_files:
+        output_txt_files[lang] = gzip.open(os.path.join(OUTPUT_DIR, '%s.txt.gz' % lang), 'at')
+        output_html_files[lang] = gzip.open(os.path.join(OUTPUT_DIR, '%s.html.gz' % lang), 'ab')
 
-    out = output_files[lang]
-    print("##SRC: %s" % filename, file=out)
-    #print("##ID: %s" % record['WARC-Record-ID'].split(':')[2][:-1], file=out)
-    print("##ID: %s" % record.rec_headers.get_header('WARC-Record-ID').split(':')[2][:-1], file=out)
-    #print("##URL: %s" % record['WARC-Target-URI'], file=out)
-    print("##URL: %s" % record.rec_headers.get_header('WARC-Target-URI'), file=out)
-    print(text+'\n', file=out)
+    out_txt = output_txt_files[lang]
+    print("##SRC: %s" % filename, file=out_txt)
+    print("##ID: %s" % record.rec_headers.get_header('WARC-Record-ID').split(':')[2][:-1], file=out_txt)
+    print("##URL: %s" % record.rec_headers.get_header('WARC-Target-URI'), file=out_txt)
+    print(text+'\n', file=out_txt)
     cntr += 1
+
+    out_html = output_html_files[lang]
+    out_html.write(bytes("##SRC: %s\n" % filename, 'utf-8'))
+    out_html.write(bytes("##ID: %s\n" % record.rec_headers.get_header('WARC-Record-ID').split(':')[2][:-1], 'utf-8'))
+    out_html.write(bytes("##URL: %s\n\n" % record.rec_headers.get_header('WARC-Target-URI'), 'utf-8'))
+    out_html.write(body+b"\n\n")
+
     #last_domain = record.rec_headers.get_header('WARC-Target-URI').split('/')[2]
 
 
-for f in output_files.values():
+for f in output_txt_files.values():
+    f.close()
+
+for f in output_html_files.values():
     f.close()
 
 
